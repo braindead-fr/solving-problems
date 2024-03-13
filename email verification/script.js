7989b4a6fc1aad0e3a5264774dbb49b5c0bd2b1e
Clinically braindead <154701961+braindead-fr@users.noreply.github.com>
2024-03-13 12:37:33 +0530

    Add files via upload

---
+++ email verification/script.js
@@ -0,0 +1,76 @@
+// email verification
+
+let emailing = [
+    'john.doe@gmail.com',
+    'jane.doe@gmail.com',
+    'bob.smith@gmail.com',
+    'alice.johnson@gmail.com',
+    'emily.brown@gmail.com',
+    'david.wilson@gmail.com',
+    'sarah.adams@gmail.com',
+    'michael.clark@gmail.com',
+    'olivia.parker@gmail.com',
+    'sam.roberts@gmail.com',
+    'carol.white@gmail.com',
+    'peter.jackson@gmail.com',
+    'lisa.thompson@gmail.com',
+    'steven.robinson@gmail.com',
+    'linda.davis@gmail.com',
+    'robert.white@gmail.com',
+    'elizabeth.hall@gmail.com',
+    'william.anderson@gmail.com',
+    'jennifer.miller@gmail.com',
+    'matt.davis@gmail.com',
+    'user123@gmail.com',
+    'abc123@gmail.com',
+    'xyz456@gmail.com',
+    'example789@gmail.com',
+    'testuser456@gmail.com',
+    'john.doe123@gmail.com',
+    'jane.smith456@gmail.com',
+    'test.email123@gmail.com',
+    'example.email456@gmail.com',
+    'user_123@gmail.com',
+    'user.abc@gmail.com',
+    'abc.xyz@gmail.com',
+    'example_123@gmail.com',
+    'testuser_456@gmail.com',
+    '123user@gmail.com',
+    '456test@gmail.com',
+    'test123@example.com',
+    'example456@example.com',
+    'user_abc@example.com',
+    'xyz_123@example.com',
+    'nigga@gmail.com',
+    'jamal@gmail.com'
+  ];
+
+let emailsearch = document.getElementById('email')
+let button = document.getElementById('button')
+
+button.addEventListener(`click`, function(){
+    let input = emailsearch.value
+    let emailexists = false
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailPattern.test(input)) {
+        for (let i = 0; i < emailing.length; i++) {
+            if (input === emailing[i]) {
+                emailexists = true
+                break;
+            }
+        }
+
+        if (emailexists) {
+            document.getElementById('emails').innerHTML = 'The email exists';
+        }
+        else{
+            document.getElementById('emails').innerHTML = 'The email does not exists'
+        }
+    }
+    else{
+        document.getElementById('emails').innerHTML = 'enter a valid email'
+    }
+
+    
+})
